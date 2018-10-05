776e1ac7c3dd034570daafbd4d85b3f4cbab6695
nzoghb <nzoghb@berkeley.edu>
2018-10-05 16:31:20 -0700

    blockchain client start

--- core/blockchain/client.py
+++ core/blockchain/client.py
@@ -1,57 +1,102 @@
-import os, inspect, sys
-from eth_utils import is_address, to_checksum_address
+import base58
+import json
+import os
 
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0,parentdir)
+import ipfsapi
 
-from core.blockchain.blockchain_utils import *
-from core.blockchain.ipfs_utils import *
+from core.configuration import ConfigurationManager
 
-abi = '''[{"constant":false,"inputs":[{"name":"name","type":"string"}],"name":"getter","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"anonymous":false,"inputs":[{"indexed":false,"name":"addr","type":"bytes32"}],"name":"NewEntry","type":"event"},{"constant":false,"inputs":[{"name":"name","type":"string"},{"name":"addr","type":"bytes32"}],"name":"setter","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"metaDb","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"}]'''
-ALPHA_URL = 'http://54.153.84.146:8545'
-ALPHA_ADDR = '0xb2CA9a58ea16599C2c827F399d07aA94b3C69dFb'
-BETA_URL = 'http://18.144.49.201:8545'
-BETA_ADDR = '0x0d65094fee55c21e256d52602a0dd6a23072223d'
-GAMMA_URL = 'http://18.144.19.67:8545'
-GAMMA_ADDR = '0x8f8301d2ac2294ad243ab7052054c3aa9a068965'
+
+logging.basicConfig(level=logging.DEBUG,
+    format='[Runner] %(asctime)s %(levelname)s %(message)s')
+
 
 class Client(object):
-	def __init__(self, nodeURL = None, nodeAddr = None):
-		if nodeURL and nodeAddr:
-			self.web3 = Web3(HTTPProvider(nodeURL))
-			self.clientAddress = nodeAddr
-		else:
-			self.web3 = Web3(HTTPProvider(ALPHA_URL))
-			self.clientAddress = ALPHA_ADDR
-		assert self.web3.isConnected()
-		# print(self.web3.personal.importRawKey('5b72313ec1c354bb06b909a8d5662452241396d62d730c72093ae053ac8bc9a0', 'boomboompow'))
-		# self.api = ipfsapi.connect('127.0.0.1', 5001)
-		CONTRACT_ADDRESS = to_checksum_address('0x39f3ba63142adf7455839fb4f91e7670a332a330')
-		print(self.clientAddress)
-		self.web3.personal.unlockAccount(self.clientAddress, 'panda')
-		# compiled_sol = compile_files('Database.sol')
-		# Database_id, Database_interface = compiled_sol.popitem()
-		self.contract_obj = self.web3.eth.contract(address=CONTRACT_ADDRESS,
-				   abi=abi)
+    #TODO: enum-like casting for multiple datatypes, if needed
+    # OBJ_TYPES = {str: IPFS._cast_str, dict: IPFS._cast_dict}
 
-	def setter(self, key, value):
-		value = json2bytes32(value)
-		tx_hash = self.contract_obj.functions.setter(key, value).transact({'from': self.clientAddress})
-		self.web3.eth.waitForTransactionReceipt(tx_hash)
-		tx_receipt = self.web3.eth.getTransactionReceipt(tx_hash)
-		return tx_receipt
+    with open("./core/blockchain/blockchain_config.json", "r") as read_file:
+        CONFIG = json.load(read_file)
 
-	def getter(self, key):
-		tx_hash = self.contract_obj.functions.getter(key).transact({'from': self.clientAddress})
-		self.web3.eth.waitForTransactionReceipt(tx_hash)
-		tx_receipt = self.web3.eth.getTransactionReceipt(tx_hash)
-		attr_dict = self.contract_obj.events.NewEntry().processReceipt(tx_receipt)
-		print(attr_dict)
-		addr = attr_dict[0]['args']['addr']
-		print(addr)
-		return bytes322json(addr)
-if __name__ == '__main__':
-	client = Client()
-	client.setter("test", "test123")
-	print(client.getter("test"))
+    def __init__(self, config_manager):
+        '''
+        TODO: Decide if `kv` is needed
+        TODO: Refactor dependencies
+        '''
+        config = config_manager.get_config()
+        self.kv = {}
+        self.client = None
+        try:
+            self.client = ipfsapi.connect(config.get("BLOCKCHAIN", "host"),
+                                            config.get("BLOCKCHAIN", "port"))
+        except Exception as e:
+            logging.info("IPFS daemon not started, got: {0}".format(e))
+
+    def setter(self, key, value):
+        
+        return tx_receipt
+
+    def getter(self, key):
+    
+        return bytes322json(addr)
+
+    def _upload_local(self, key: str, obj: object):
+        '''Test method, DO NOT USE
+        '''
+        addr = self.client.add(obj)
+        self.kv[key] = addr
+
+    def _download_local(self, key: str) -> object:
+        '''Test method, DO NOT USE
+        '''
+        addr = self.kv[key]
+        return self.client.get(addr)
+
+    def _upload(self, obj: object) -> str:
+        '''Provided any Python object, store it on IPFS and then upload
+        the hash to the blockchain
+        TODO: blockchain setter API required
+        '''
+        ipfs_hash = self._content_to_ipfs(obj)
+        byte_addr = self._ipfs_to_blockchain(ipfs_hash)
+        return addr
+
+    def _download(self, byte_addr: bytes) -> object:
+        '''Provided an on-chain content address, retrieve the Python
+        object from IPFS
+        TODO: blockchain getter API required
+        '''
+        ipfs_hash = self._blockchain_to_ipfs(byte_addr)
+        content = self._ipfs_to_content(ipfs_hash)
+        return content
+
+    def _ipfs_to_content(self, ipfs_hash: str) -> object:
+        '''Helper function to retrieve a Python object from an IPFS hash
+        '''
+        return self.client.get(addr)
+
+    def _content_to_ipfs(self, obj: object) -> str:
+        '''Helper function to deploy a Python object onto IPFS, 
+        returns an IPFS hash
+        '''
+        return self.client.add(obj)
+
+    def _ipfs_to_blockchain(self, ipfs_hash: str) -> bytes:
+        '''Helper function to convert IPFS hashes to on-chain content addresses
+        '''
+        return base58.b58encode(b'\x12 ' + ipfs_hash)
+
+    def _blockchain_to_ipfs(self, byte_addr: bytes) -> str:
+        '''Helper function to convert on-chain content addresses to IPFS hashes
+        '''
+        return base58.b58decode(byte_addr)[2:]
+
+    def _cast_str(self, input: str):
+        '''Helper function to cast string inputs to desired type
+        '''
+        pass
+
+    def _cast_dict(self, input: dict):
+        '''Helper function to cast dict inputs to desired type
+        '''
+        pass
