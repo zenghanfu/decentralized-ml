1d4b215fe7caac1e400b01dd57343f0e4c8fa0e4
kiddyboots216 <kiddyboots216@berkeley.edu>
2018-11-11 14:49:21 -0800

    Cleaning and updated type hints

--- tests/test_blockchain_gateway.py
+++ tests/test_blockchain_gateway.py
@@ -49,7 +49,7 @@ def test_blockchain_gateway_can_listen_decentralized_learning(config_manager, co
     blockchain_gateway.configure(config_manager, communication_manager)
     tx_receipt = setter(blockchain_gateway.client, None, blockchain_gateway.port, {"model": "hello world"}, True)
     assert tx_receipt
-    blockchain_gateway.listen(blockchain_gateway._handle_new_session_creation,
+    blockchain_gateway._listen(blockchain_gateway._handle_new_session_creation,
         blockchain_gateway._filter_new_session)
     # at this point we should listen for decentralized learning, hear it, and update our communication manager
     assert communication_manager.dummy1 == RawEventTypes.NEW_SESSION.name, "Wrong dummy1"
